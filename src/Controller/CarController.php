00b86ca0cae2c2f8d818520ed8c5d4bd52698c6a
zacube <contact@zacube.com>
2025-09-30 19:24:23 +0200

    Mise en place CarController et fichier html.twig correspondant

---
+++ src/Controller/CarController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CarRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class CarController extends AbstractController
+{
+    #[Route('/car', name: 'app_car')]
+    public function index(CarRepository $carRepository): Response
+    {
+        $cars = $carRepository->findAll();
+
+/*        $output = '<h1>Liste des voitures</h1><ul>';
+        foreach ($cars as $car) {
+            $output .= '<li>' . $car->getName() . ' ' . $car->getSeats() . '</li>';
+        }
+        $output .= '</ul>';*/
+
+        return $this->render('car/index.html.twig', [
+            'controller_name' => 'CarController',
+            'cars' => $cars
+
+        ]);
+    }
+
+}
+
